a99a8ae2bfb66801d1f9fa9a1be3f4dfd476fba7
Akhil Vardhan <akhilvardhan27@gmail.com>
2022-06-13 13:23:40 +0400

    second commit

--- client.js
+++ client.js
@@ -3,53 +3,62 @@ const socket = io.connect('http://localhost:8080');
 const form = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 const messageContainer = document.querySelector(".container");
-const buttons = document.querySelectorAll('.my-button-text');
-
-message.document.designMode = 'On';
-
-for(let i=0;i<buttons.length;i++) {
-    buttons[i].addEventListener('click', ()=> {
-        let cmd = buttons[i].getAttribute('data-cmd');
-        if(buttons[i].name === "active") {
-            buttons[i].classList.toggle('active');
-        }
-        if(cmd === "createLink") {
-            let url = prompt("Enter link here : ");
-            message.document.execCommand(cmd,false,url);
-        }
-        else {
-            message.document.execCommand(cmd,false,null);
-        }
-    })
-}
+const buttons = document.querySelectorAll(".my-button-text");
 
 const append = (message, position)=> {
     const messageElement = document.createElement('div');
-    messageElement.innerText = message;
+    messageElement.innerHTML = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
 }
 
-const name = prompt("Enter Your Name to join the live chat");
-socket.emit('new-user-joined',name);
+const user_name = prompt("Enter Your Name to join the live chat");
+socket.emit('new-user-joined',user_name);
 
 socket.on('user-joined',name => {
-    append(`${name} has joined the chat`,middle);  
+    append(`${name} has joined the chat`,'middle');  
 });
 
-socket.on('receive',data => {
+socket.on('recieve',data => {
     append(`${data.name}: ${data.message}`,'left');
 });
 
 socket.on('left',name => {
     append(`${name} left the chat`,'middle');
-})
+});
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const message = messageInput.value;
+    let iDocument = messageInput.contentWindow.document;
+    let message = iDocument.body.innerHTML;
+    console.log(message);
     append(`You: ${message}`,'right');
     socket.emit('send',message);
-    messageInput.value;
-})
+    messageInput.contentWindow.document = "";
+});
+
+messageName.document.designMode = 'On';
+
+buttons.forEach( button => {
+    button.addEventListener('click', () => {
+        console.log("working");
+        let command = button.getAttribute('data-cmd');
+        if(command === "createLink" || command === "addFile") {
+            let url = prompt("Enter link here : ");
+            messageName.document.execCommand("createLink",false,url);
+        }
+        else if(command === "addAt") {
+            let person = prompt("Enter the persons name : ");
+            let iDocument = messageInput.contentWindow.document.body;
+            iDocument.appendChild(`<p>@ ${person}</p>`);
+        }
+        else if(command === "addEmoji") {
+            let iDocument = messageInput.contentWindow.document.body;
+            iDocument.appendChild("<p>&#128516</p>");
+        }
+        else {
+            messageName.document.execCommand(command,false,null);
+        }
+    })
+});
--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
-const io = require('socket.io')(8080);
+const io = require('socket.io')(8080, {
+    cors: {
+        origin: ['http://127.0.0.1:5500']
+    }
+});
 
 const users = {};
 
 io.on('connection',socket => {
-    socket.on('new-user-joined',name => {
-        console.log("The person is ",name);
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined',name);
+    socket.on('new-user-joined',user_name => {
+        console.log('new user :  ',user_name);
+        users[socket.id] = user_name;
+        socket.broadcast.emit('user-joined',user_name);
     });
     socket.on('send',message => {
         socket.broadcast.emit('recieve', {message : message, name : users[socket.id]});
     });
-    socket.on('disconnect',message =>{
+    socket.on('disconnect',message => {
         socket.broadcast.emit('left',users[socket.id]);
         delete users[socket.id];
     });
